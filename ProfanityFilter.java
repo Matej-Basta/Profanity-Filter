664e1801f3d501eb2e96cdd1d4339cd33b452364
matej <matej.basta@email.cz>
2022-09-30 15:42:01 +0200

    commenting the profanityFilter class

--- ProfanityFilter.java
+++ ProfanityFilter.java
@@ -18,24 +18,20 @@ public class ProfanityFilter {
         List<String> tweet = inputReader.getRest();
         List<String> swearWords = inputReader.getSwearWords();
         
+        //looping through every line in a tweet
         for (int i = 0; i < tweet.size(); i++) {
 
             String line = tweet.get(i);
             String[] arrayOfWords = getArrayOfWords(line);
 
+            //looping through every word in a line
             for (int j = 0; j < arrayOfWords.length; j++) {
 
+                //looping through every swear word
                 for (int k = 0; k < swearWords.size(); k++) {
                     
                     if (arrayOfWords[j].toLowerCase().replaceAll("[,.?!]", "").equals(swearWords.get(k))) {
-                        String[] individualWord = arrayOfWords[j].split("");
-                        for (int l = 0; l < individualWord.length; l++) {
-                            if (!individualWord[l].equals(".") && !individualWord[l].equals(",") && !individualWord[l].equals("?") && !individualWord[l].equals("!")) {
-                                individualWord[l] = characters[l % 5];
-                            }     
-                        }
-                        String changedWord = String.join("", individualWord);
-                        arrayOfWords[j] = changedWord;
+                        arrayOfWords[j] = censorSwearWord(arrayOfWords[j]);
                     }
                 }
             }
@@ -46,6 +42,26 @@ public class ProfanityFilter {
         }
     }
 
+    /* 
+     * Taking a word and replacing every character (except .,?!) with a symbol
+     * @param word The word that is supposed to be replaced.
+     * @return String Word, whose letters were replaced by symbols.
+     */
+    private String censorSwearWord(String word) {
+        String[] letters = word.split("");
+        for (int l = 0; l < letters.length; l++) {
+            if (!letters[l].equals(".") && !letters[l].equals(",") && !letters[l].equals("?") && !letters[l].equals("!")) {
+                letters[l] = characters[l % 5];
+            }
+        }
+        String censoredWord = String.join("", letters);
+        return censoredWord;
+    }
+
+    /* 
+     * Takes every word in a line and places it into a list
+     * @param string an individual line of a tweet
+     */
     private String[] getArrayOfWords(String string) {
         String[] arrayOfWords = string.split(" ");
         return arrayOfWords;
